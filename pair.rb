bcc549d4ce3d54eb838c7e26f30e068eed7c992a
Denise Nguyen <Denise.Nguyen224@gmail.com>
2014-09-17 16:14:31 -0400

    attr_accessor

--- pair.rb
+++ pair.rb
@@ -6,17 +6,7 @@ class Pair
 	end
 
 	attr_reader :key
-	attr_reader :value
-	
-	attr_writer :value	
-	attr_writer :value
-	#def key=(new_key)
-	#	@key = new_key
-	#end
-
-	#def value=(new_value)
-	#	@value = new_value
-	#end
+	attr_accessor :value 
 
 end
 
